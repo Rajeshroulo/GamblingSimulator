019e1a9e59010f5c64dae111e7c0067c55161343
Rajeshroulo <rajraval017@gmail.com>
2020-12-05 15:28:16 +0530

    Added code to check the outcome of the game

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -1,5 +1,24 @@
 class GamblingSimulation {
+    stake=100;
+    bet=1;
 
+    outcome()
+    {
+        var random = Math.floor(Math.random() * 2);
+        if(random==1)
+        {
+            console.log("Win the match");
+            this.stake=this.stake+this.bet;
+            console.log(this.stake);
+        }
+        else
+        {
+            console.log("Lost the match");
+            this.stake=this.stake-this.bet;
+            console.log(this.stake);
+        }
+    }
+    
     readvalues() {
         const readline = require('readline').createInterface({
             input: process.stdin,
@@ -9,10 +28,22 @@ class GamblingSimulation {
     }
 
     enterStakeandBetValue(read) {
-
+        let that=this;
+        this.read=read;
         read.question("Enter stake value :", function (stake) {
             read.question("Enter bet value :", function (bet) {
+                that.enterTrialsandGoalValue(read);
                 console.log("the stake is:"+stake+" ,bet is:"+bet);
+            });
+
+         });
+    }
+
+    enterTrialsandGoalValue(read) {
+
+        read.question("Enter trials  :", function (trials) {
+            read.question("Enter goal :", function (goal) {
+                console.log("the trials is:"+trials+" ,goal is:"+goal);
                 read.close();
             });
 
@@ -22,6 +53,5 @@ class GamblingSimulation {
 }
 
 let gambler = new GamblingSimulation();
-var read = gambler.readvalues();
-gambler.enterStakeandBetValue(read);
+gambler.outcome();
 
