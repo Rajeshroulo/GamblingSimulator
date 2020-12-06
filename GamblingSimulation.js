bc8d21c8d3b01b9821832545b07e56bd7937326d
Rajeshroulo <rajraval017@gmail.com>
2020-12-06 05:37:14 +0530

    Refactored the code with promises in the functions

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -1,57 +1,148 @@
-class GamblingSimulation {
-    stake=100;
-    bet=1;
+/**
+ * Class Declaration
+ * 
+ * @class  GamblingSimulation
+ * @description  Computing Gambling Simulation 
+ *
+ */
 
-    outcome()
-    {
-        var random = Math.floor(Math.random() * 2);
-        if(random==1)
-        {
-            console.log("Win the match");
-            this.stake=this.stake+this.bet;
-            console.log(this.stake);
+class GamblingSimulation {
+    wins = 0;
+    loss = 0;
+    readline;
+
+    /**
+     * 
+     * @method outcome
+     * to check the wins and loss of the game 
+     */
+    outcome(stake,bet,goal,trials) {
+        var bets = 0;
+        this.money = stake;
+        for (var t = 0; t < trials; t++) {
+            while (this.money > 0 && this.money < goal) {
+                bets++;
+                var random = Math.floor(Math.random() * 2);
+                if (random == 1) {
+                    this.money = parseInt(this.money) + parseInt(bet);
+                }
+                else {
+                    this.money = parseInt(this.money) - parseInt(bet);
+                }
+
+            }
+
+            if (this.money == goal) {
+                this.wins++;
+            }
+
+            else {
+                this.loss++;
+            }
         }
-        else
-        {
-            console.log("Lost the match");
-            this.stake=this.stake-this.bet;
-            console.log(this.stake);
-        }
+
+        console.log(this.wins + " matches won out of " + trials);
+        console.log("final money player have :" + this.money);
     }
-    
-    readvalues() {
-        const readline = require('readline').createInterface({
+
+    /**
+     * @constructor
+     * @description Declaring readline object
+     */
+    constructor() {
+        this.readline = require('readline').createInterface({
             input: process.stdin,
             output: process.stdout
         });
-        return readline;
     }
 
-    enterStakeandBetValue(read) {
+    /**
+     * @method playGame
+     * calling the promise in the function
+     */
+
+    playGame() {     
+      this.stakeValue()
+      .then((stake)=>{
+        console.log(stake);
+        return this.betValue();
+      })
+      .then((bet)=>{
+        console.log(bet);
+        return this.goalValue();
+      })
+      .then((goal)=>{
+        console.log(goal);
+        return this.trialValue();
+      })
+      .then((trials)=>{
+        console.log(trials);
+        return this.outcome();
+      })
+
+      .catch(error => { 
+        console.error("please enter proper values");
+      });   
+    }
+
+    stakeValue() {
         let that=this;
-        this.read=read;
-        read.question("Enter stake value :", function (stake) {
-            read.question("Enter bet value :", function (bet) {
-                that.enterTrialsandGoalValue(read);
-                console.log("the stake is:"+stake+" ,bet is:"+bet);
+        return new Promise((resolve, reject) => {
+            that.readline.question("Enter stake value :", (stake) => {
+                if(typeof stake === undefined || isNaN(stake) ){
+                    return reject("Please enter valid stake")
+                }
+                else{
+                    return resolve(stake);
+                }
             });
-
-         });
+        });
     }
 
-    enterTrialsandGoalValue(read) {
-
-        read.question("Enter trials  :", function (trials) {
-            read.question("Enter goal :", function (goal) {
-                console.log("the trials is:"+trials+" ,goal is:"+goal);
-                read.close();
+    betValue() {
+        let that=this;
+        return new Promise((resolve, reject) => {
+            that.readline.question("Enter bet value :", (bet) => {
+                if(typeof bet === undefined || isNaN(bet) ){
+                    return reject("Please enter valid bet")
+                }
+                else{
+                    return resolve(bet);
+                }
             });
-
-         });
+        });  
+    
     }
-            
+
+    goalValue(){
+        let that=this;
+        return new Promise((resolve, reject) => {
+            that.readline.question("Enter goal value :", (goal) => {
+                if(typeof goal === undefined || isNaN(goal) ){
+                    return reject("Please enter valid goal")
+                }
+                else{
+                    return resolve(goal);
+                }
+            });
+        });
+
+    }
+
+    trialValue(){
+        let that=this;
+        return new Promise((resolve, reject) => {
+            that.readline.question("Enter trials value :", (trials) => {
+                if(typeof trials === undefined || isNaN(trials) ){
+                    return reject("Please enter valid trials")
+                }
+                else{
+                    return resolve(trials);
+                }
+            });
+        });
+    }                
 }
 
 let gambler = new GamblingSimulation();
-gambler.outcome();
-
+gambler.playGame();
